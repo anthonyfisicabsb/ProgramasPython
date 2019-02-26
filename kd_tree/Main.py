1b146b8fc3e3b1b378b514e9d5d2a519ea4a5a9b
Anthony Louis <anthony.physis@gmail.com>
2019-02-26 20:42:46 -0300

    exercicio de kdtree

---
+++ kd_tree/Main.py
@@ -0,0 +1,13 @@
+import random
+from kdtree.Kd_Tree import Kd_Tree
+from kdtree.Node import Node
+
+if __name__ == '__main__':
+    root = Node([random.randint(-1000, 1001), random.randint(-1000, 1001)], True)
+    kd_tree = Kd_Tree(root)
+
+    for i in range(1):
+        node = Node([random.randint(-1000, 1001), random.randint(-1000, 1001)], True)
+        kd_tree.add_node(node)
+
+    kd_tree.print_tree(root)
